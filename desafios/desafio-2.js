d85de0807d21796fd62b60619dd059cc13b8380e
FacundoJoelRodr <soyfacujoelrodriguez@gmail.com>
2023-09-28 19:30:28 -0300

    se agrega cambios sugeridos por tutor

--- desafios/desafio-2.js
+++ desafios/desafio-2.js
@@ -1,33 +1,48 @@
-const fs = require('fs');
+const fs = require("fs");
 
 class ProductManager {
-  
   constructor(path) {
     this.products = [];
-    this.nextProductId = 1;
-    this.path = path;   
+    this.path = path;
   }
- 
+
   getProducts() {
-    return getJsonFromFile(this.path)
+    return getJsonFromFile(this.path);
   }
 
- async addProduct(productAdd) {
-      const {title, code, thumbnail, price, stock , description } = productAdd;
-      const products = await getJsonFromFile(this.path);
+  async addProduct(productAdd) {
+    const { title, code, thumbnail, price, stock, description } = productAdd;
+    const products = await getJsonFromFile(this.path);
 
-    if (!productAdd.title || !productAdd.code || !productAdd.thumbnail || !productAdd.price || !productAdd.stock || !productAdd.description) {
+    if (
+      !productAdd.title ||
+      !productAdd.code ||
+      !productAdd.thumbnail ||
+      !productAdd.price ||
+      !productAdd.stock ||
+      !productAdd.description
+    ) {
       console.log(`Faltan campos obligatorios`);
       return;
-    } 
-    if (products.some(products => products.code === productAdd.code)) {
-      console.log("Este producto ya se encuentra en el array y no se va a agregar");
+    }
+    if (products.some((products) => products.code === productAdd.code)) {
+      console.log(
+        "Este producto ya se encuentra en el array y no se va a agregar"
+      );
     } else {
       const id = products.length + 1;
-      const newProduct = {id,title, code, thumbnail, price, stock , description}
-      products.push(newProduct)
+      const newProduct = {
+        id,
+        title,
+        code,
+        thumbnail,
+        price,
+        stock,
+        description,
+      };
+      products.push(newProduct);
       console.log("Se agregó correctamente el producto");
-      return saveJsonToFile(this.path,products);
+      return saveJsonToFile(this.path, products);
     }
   }
 
@@ -36,141 +51,178 @@ class ProductManager {
     const product = products.find((p) => p.id === productId);
 
     if (!product) {
-       console.log(`no se encontro el id ${productId} solicitado`);
-    } else {
-       return product
+      console.log(`no se encontro el id ${productId} solicitado`);
+    }
+    return product;
+  }
+
+  async updateProduct(productId, updatedProduct) {
+    const products = await getJsonFromFile(this.path);
+    let { title, code, thumbnail, price, stock, description } = updatedProduct;
+    const product = products.findIndex((p) => p.id === productId);
+    if (product === -1) {
+      console.log(`no se encontro el id ${productId} solicitado if`);
+      return null;
+    }
+
+    const properties = [
+      "title",
+      "code",
+      "thumbnail",
+      "price",
+      "stock",
+      "description",
+    ];
+    for (const prop in updatedProduct) {
+      if (!properties.includes(prop)) {
+        console.log(`La propiedad ${prop} no es permitida`);
+        return null;
+      }
+    }
+
+    if (
+      !updatedProduct.title ||
+      !updatedProduct.code ||
+      !updatedProduct.thumbnail ||
+      !updatedProduct.price ||
+      !updatedProduct.stock ||
+      !updatedProduct.description
+    ) {
+      console.log(`Faltan campos obligatorios`);
+      return;
+    }
+    
+    let newUpdatedProduct = {
+      title,
+      code,
+      thumbnail,
+      price,
+      stock,
+      description,
+      id: productId,
+    };
+
+    products[product] = {
+      ...products[product],
+      ...newUpdatedProduct,
+      id: productId,
+    };
+
+    try {
+      await saveJsonToFile(this.path, products);
+      console.log(`Producto con ID ${productId} actualizado exitosamente`);
+      return products[product];
+    } catch (error) {
+      console.error("Error al guardar los cambios:", error.message);
+      return null;
     }
   }
 
-  async updateProduct(productId, updatedProduct){
+  async deleteProduct(productId) {
     const products = await getJsonFromFile(this.path);
     const product = products.findIndex((p) => p.id === productId);
     if (product === -1) {
       console.log(`no se encontro el id ${productId} solicitado if`);
       return null;
-   } 
-   products[product] = {
-    ...products[product], 
-    ...updatedProduct, 
-    id: productId, 
-  };
+    }
+    products.splice(product, 1);
 
-  try {
-    await saveJsonToFile(this.path, products);
-    console.log(`Producto con ID ${productId} actualizado exitosamente`);
-    return products[productIndex]; 
-  } catch (error) {
-    console.error('Error al guardar los cambios:', error.message);
-    return null;
+    try {
+      await saveJsonToFile(this.path, products);
+      console.log(`Producto con ID ${productId} eliminado exitosamente`);
+      return productId;
+    } catch (error) {
+      console.error("Error al guardar los cambios:", error.message);
+      return null;
+    }
   }
- }
-
- async deleteProduct(productId){
-  const products = await getJsonFromFile(this.path);
-  const product = products.findIndex((p) => p.id === productId);
-  if (product === -1) {
-    console.log(`no se encontro el id ${productId} solicitado if`);
-    return null;
- } 
- products.splice(product, 1);
-
- try {
-   await saveJsonToFile(this.path, products);
-   console.log(`Producto con ID ${productId} eliminado exitosamente`);
-   return productId; 
- } catch (error) {
-   console.error('Error al guardar los cambios:', error.message);
-   return null;
- }
 }
 
-}
 // Funciones de archivo JSON
 
-
 //SI EXISTE EL ARCHIVO JSON
-const existFile = async (path)=>{
+const existFile = async (path) => {
   try {
-   await fs.promises.access(path);
-    return true
+    await fs.promises.access(path);
+    return true;
   } catch (error) {
-    return false
+    return false;
   }
- }
+};
 
- //OBTENER EL ARCHIVO JSON
+//OBTENER EL ARCHIVO JSON
 
 const getJsonFromFile = async (path) => {
-  if(!await existFile(path)){
+  if (!(await existFile(path))) {
     return [];
   }
   let content;
 
-  try{
-     content = await fs.promises.readFile(path,'utf-8')
-  }catch(error){
-    throw new Error('El archivo lamentablemente no pudo ser leido')
-  }
- 
   try {
-    return JSON.parse(content)
+    content = await fs.promises.readFile(path, "utf-8");
   } catch (error) {
-    throw new Error('El archivo no tiene un formato JSON correcto')
+    throw new Error("El archivo lamentablemente no pudo ser leido");
   }
-}
+
+  try {
+    return JSON.parse(content);
+  } catch (error) {
+    throw new Error("El archivo no tiene un formato JSON correcto");
+  }
+};
 
 //GUARDAR ARCHIVO JSON
 const saveJsonToFile = async (path, data) => {
-  const content = JSON.stringify(data,null, '\t');
+  const content = JSON.stringify(data, null, "\t");
   try {
-    await fs.promises.writeFile(path,content, 'utf-8');
+    await fs.promises.writeFile(path, content, "utf-8");
   } catch (error) {
-    throw new Error('El archivo lamentablemente no pudo ser guardado')
+    throw new Error("El archivo lamentablemente no pudo ser guardado");
   }
-}
+};
 
+(async function (run) {
+  if (!run) return;
 
-
-
-(async function(run){
-  if(!run) return;
-
-  const productManager = new ProductManager('./productos.json');
+  const productManager = new ProductManager("./productos.json");
   //SE AGREGA PRODUCTO
   await productManager.addProduct({
-    title: 'producto prueba',
-    description: 'Este es un producto prueba',
+    title: "producto prueba",
+    description: "Este es un producto prueba",
     price: 200,
-    thumbnail: 'Sin imagen',
-    code: 'abc123',
+    thumbnail: "Sin imagen",
+    code: "abc123",
     stock: 25,
   });
-   
+
   //SE OBTIENE LOS PRODUCTOS POR ID Y EN GENERAL
   const users1 = await productManager.getProducts();
-  console.log('el listdo del los productos es: ', users1)
+  console.log("el listdo del los productos es: ", users1);
 
   const user1 = await productManager.getProductById(1);
-  console.log('el producto por el id es: ', user1)
+  console.log("el producto por el id es: ", user1);
 
   // SE ACTUALIZA EL PRODUCTO
-  const cambio =  {title: 'update2',
-  description: 'Este es un producto prueba de update',
-  price: 200,
-  thumbnail: 'Sin imagen',
-  code: 'abc123',
-  stock: 25}
+
+  const cambio = {
+    title: "update24",
+    description: "Este es un producto prueba de update",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: "abc123",
+    stock: 25,
+    mila: "mila",
+  };
 
   await productManager.updateProduct(1, cambio);
 
-  await productManager.deleteProduct(1)
+  //await productManager.deleteProduct(1)
 
   //SE OBTIENE LOS PRODUCTOS POR ID Y EN GENERAL
-  
-  const users = await productManager.getProducts();
-  console.log('el listdo del los productos es: ', users)
 
-  const user = await productManager.getProductById(1);
-  console.log('el producto por el id es: ', user)
-})(true)
+  //const users = await productManager.getProducts();
+  // console.log('el listdo del los productos es: ', users)
 
+  //const user = await productManager.getProductById(1);
+  //console.log('el producto por el id es: ', user)
+})(true);
